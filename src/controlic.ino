815f8936685f25197dbc91030feac98017bb269d
gDiode <a.g.ben.min@gmail.com>
2021-12-23 20:25:58 -0600

    Update controlic.ino
    functional now

--- src/controlic.ino
+++ src/controlic.ino
@@ -1,14 +1,12 @@
-int phaseControlPin = 3; 
+int phaseControlPin = 1; 
 int analogPin = A0;
-int triggPin =  4;
+int triggPin =  2;
 int currentState = 0;
 int previousState = 0;
-int controll=0;
-
-
-
+//int controll=0;
 
 void setup() {
+   //Serial.begin(9600);
    pinMode(phaseControlPin, OUTPUT);
    pinMode(triggPin, INPUT);
   // Serial.begin(9600);
@@ -17,24 +15,31 @@ void setup() {
 void dimmer(int percentage, int outputPin){
 //entrada 0-1023 en percentage
 //120hz = 0.00833s = 8.33ms = 8333us
+//lower percentage value ==higher power
 int a = 0;
-if(percentage>=1){
+if(percentage<=3){
   digitalWrite(outputPin,HIGH);
-  //rango de operacion de delayUs 3-8332us
-  a = percentage*(8.1446); //el numero magico se obtiene de 8332/1023
-  //Serial.println(a);
-  delayMicroseconds(a);
+}
+else if(percentage>850){//valor obtenido en prueba
   digitalWrite(outputPin,LOW);
 }
- else
-{
+else{//respuesta linear
   digitalWrite(outputPin,LOW);
+  //rango de operacion de delayUs 3-8332us
+  a = percentage*(8.0482); //el numero magico se obtiene de 8332/1023
+  //Serial.println(a);
+  delayMicroseconds(a);
+  digitalWrite(outputPin,HIGH);
 }
 }
 
 void loop() {
+   
    int set = 0;
-   currentState=digitalRead(triggPin); 
+
+   currentState=digitalRead(triggPin);
+   //Serial.println(analogRead(analogPin));
+   
    if(currentState!=previousState){
     set = analogRead(analogPin);
     dimmer(set,phaseControlPin);
